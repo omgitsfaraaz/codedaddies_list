959784aadecee5ff401fe92014561dd5f82098fe
unknown <mohsinsait23@gmail.com>
2020-10-11 13:02:35 +0530

    search query getting added to search model in backend

--- codedaddies_list/my_app/views.py
+++ codedaddies_list/my_app/views.py
@@ -2,6 +2,7 @@ import requests
 from requests.compat import quote_plus
 from django.shortcuts import render
 from bs4 import BeautifulSoup
+from . import models
 
 BASE_CRAIGLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 
@@ -10,10 +11,11 @@ def home(request):
 
 def new_search(request):
 	search = request.POST.get('search')
+	models.Search.objects.create(search=search)
 	#print(quote_plus(search))
 	final_url = BASE_CRAIGLIST_URL.format(quote_plus(search))
 	print(final_url)
-	response = requests.get('https://losangeles.craigslist.org/search/?query=python%20tutor')
+	response = requests.get(final_url)
 	data = response.text
 	#print(data)
 	stuff_for_frontend = {
