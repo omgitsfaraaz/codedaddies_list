cebeb810905fad4e82dc98fb5518e0f42e10bd4b
unknown <mohsinsait23@gmail.com>
2020-10-11 12:28:13 +0530

    fetched the search query and viewed in cmd

--- codedaddies_list/my_app/views.py
+++ codedaddies_list/my_app/views.py
@@ -1,10 +1,17 @@
+import requests
 from django.shortcuts import render
+from bs4 import BeautifulSoup
+
+BASE_CRAIGLIST_URL = 'https://losangeles.craigslist.org/search/?query={}'
 
 def home(request):
 	return render(request, 'base.html')
 
 def new_search(request):
 	search = request.POST.get('search')
+	response = requests.get('https://losangeles.craigslist.org/search/?query=python%20tutor')
+	data = response.text
+	print(data)
 	stuff_for_frontend = {
 		'search': search,
 	}
